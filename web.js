6680bd43ff95a182ed5e0e556da72af8ec6ac260
rcpeters <info@rcpeters.com>
2013-07-18 21:14:18 -0700

    better copy

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express')
    , fs = require('fs')
-   , buffer = require('buffer');
+   , buffer = require('buffer')
+   , path = require('path');
 
 var app = express.createServer(express.logger());
 
 var indexHtml = fs.readFileSync('index.html');
+
+app.use("/static", express.static(__dirname + '/static'));
 
 app.get('/', function(request, response) {
   response.send(indexHtml.toString());
